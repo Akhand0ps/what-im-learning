a3236e7bd157e8e6718b0e484ab79a5552695604
Akhand0ps <akhandps08@gmail.com>
2025-05-25 16:35:28 +0530

    learning typescript

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+"use strict";
+let userName = "Akhand";
+console.log(`Hello, ${userName}`);
+let a = 1;
+console.log(a);
+function greet(firstname) {
+    console.log("hello " + firstname);
+}
+greet("akhan    d");
---
+++ backend/index.ts
@@ -0,0 +1,15 @@
+let userName: string = "Akhand";
+console.log(`Hello, ${userName}`);
+
+
+
+let a: number = 1;
+console.log(a);
+
+
+function greet(firstname:string){
+    console.log("hello "+ firstname);
+}
+
+
+greet("akhand");
